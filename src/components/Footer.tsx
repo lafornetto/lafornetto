360f120c3d867886f0df2d7c9be90e79f36dce16
Johan Persson <graniten40@gmail.com>
2026-06-10 01:32:04 +0200

    Update QR code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,34 @@
+import qrCode from "../assets/images/url-qr-code.png";
+
 export function Footer() {
   return (
     <footer className="site-footer">
-      <p>© 2026 La Fornetto Älvkarleby. Alla rättigheter förbehållna.</p>
+      <div className="footer-left">
+        <p>© 2026 La Fornetto Älvkarleby. Alla rättigheter förbehållna.</p>
+      </div>
 
       <div className="footer-links">
         <a
-            href="https://www.facebook.com/profile.php?id=100054200664793"
-            target="_blank"
-            rel="noreferrer"
+          href="https://www.facebook.com/profile.php?id=100054200664793"
+          target="_blank"
+          rel="noreferrer"
         >
-            Facebook
+          Facebook
         </a>
 
         <a
-            href="https://www.instagram.com/lafornetto2025/"
-            target="_blank"
-            rel="noreferrer"
+          href="https://www.instagram.com/lafornetto2025/"
+          target="_blank"
+          rel="noreferrer"
         >
-            Instagram
+          Instagram
         </a>
-        </div>
+      </div>
+
+      <div className="footer-qr">
+        <p>Skanna för meny</p>
+        <img src={qrCode} alt="QR-kod till menyn" />
+      </div>
     </footer>
   );
 }
