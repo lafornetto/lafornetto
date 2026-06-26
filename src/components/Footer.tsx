d210b4f3be905b36df98392c31233d4388487bbd
Johan Persson <graniten40@gmail.com>
2026-06-26 16:38:28 +0200

    Update Facebook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export function Footer({ t }: FooterProps) {
 
       <div className="footer-links">
         <a
-          href="https://www.facebook.com/profile.php?id=100054200664793"
+          href="https://www.facebook.com/profile.php?id=61575863934833"
           target="_blank"
           rel="noreferrer"
         >
